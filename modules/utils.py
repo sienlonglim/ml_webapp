99bd414b33c8fc0c4fff8079b98185e85328ffc5
sienlonglim <limsienlong@gmail.com>
2024-03-07 15:13:22 +0800

    changed logger to append mode

--- modules/utils.py
+++ modules/utils.py
@@ -8,7 +8,7 @@ from functools import wraps
 # logging.config.fileConfig(fname='logging_config.ini', disable_existing_loggers=False)
 # logger = logging.getLogger('debugger')
 
-def add_custom_logger(name, file_path=None, write_mode='w' , streaming=None, level=logging.INFO):
+def add_custom_logger(name, file_path=None, write_mode='a' , streaming=None, level=logging.INFO):
     '''
     Initiates the logger
     '''
